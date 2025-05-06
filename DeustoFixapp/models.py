7aa59c332f05f0730d229017cb82a28069f4e8c5
Hezitzaile <juanmaria.diaz@opendeusto.es>
2025-05-06 11:38:36 +0200

    Creacion de modelos. Se añaden a Admin.py para registrar los modelos. Se realizan las migraciones para aplicar los cambios. A partir de aqui se crea el superuser, se arranca el servidor y se comprueba que efectivamente funciona de manera correcta pudiendo añadir todo tipo de objetos.

--- DeustoFixapp/models.py
+++ DeustoFixapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Instalacion(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=50)
+    tipo_uso = models.CharField(max_length=50, blank=True)
+    ubiacion = models.CharField(max_length=100)
+    capacidad =  models.IntegerField(null=True, blank=True)
+    estado = models.CharField(max_length=20, default='Activa')
+    departamento_responsable = models.CharField(max_length=100)
+    
+    def __str__ (self):
+        return self.nombre
+    
+class Empleado(models.Model):
+    dni = models.CharField(max_length=9, unique=True)
+    
