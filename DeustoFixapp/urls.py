69ac269ef28e70416f6e2feb17273c61e0616cd8
Michael Hindle <michael.hindle@opendeusto.es>
2025-05-06 20:01:49 +0200

    Colocar en view.py la lista, detalle, actualizar, eliminar y visualizacionde incidencia. Poner en urls.py las Urls de incidencia. Añadir los templates de incidencia y asegurar que en el settings detecte la carpeta de los templates.

--- DeustoFixapp/urls.py
+++ DeustoFixapp/urls.py
@@ -1,9 +1,23 @@
-
-
 from django.urls import path
 from . import views
+from .views import (MenuPrincipalView, IncidenciaMenuView, IncidenciaListView, IncidenciaCreateView,
+                    IncidenciaUpdateView, IncidenciaDeleteView)
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('incidencia', views.Incidencia, name='')
+    # menu principal.
+
+    path('', MenuPrincipalView.as_view, name='menu_principal'),
+
+    # urls de incidencia.
+
+    path('menu_incidencia', IncidenciaMenuView, name='incidencia'),
+    path('menu_incidencia', IncidenciaMenuView.as_view(), name='incidencia_menu'),
+    path('menu_incidencia', IncidenciaListView.as_view(), name='incidencia_menu'),
+    path('create_incidencia', IncidenciaCreateView.as_view(), name='incidencia_create'),
+    path('update_incidencia/<int:pk>', IncidenciaUpdateView.as_view(), name='update_incidencia'),
+    path('delete_incidencia/<int:pk>', IncidenciaDeleteView.as_view(), name='delete_incidencia'),
+
+    # urls de Instalacion
+
+    # urls de Empleados
 ]
--- DeustoFixapp/views.py
+++ DeustoFixapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import View, ListView
+from django.db.models import Q
+from django.http import request, JsonResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import View, ListView, DetailView
 from django.urls import reverse
 from django.views.generic import TemplateView
 
@@ -9,19 +11,93 @@ from DeustoFixapp.models import Incidencia
 
 # Create your views here.
 
+# La view del menu principal
+
 class MenuPrincipalView(View):
     def get(self, request):
         return render(request, "inicio.html")
 
-#Incidencia
 
-class IncidenciaListViews(ListView):
+# Las views de Incidencia
+class IncidenciaMenuView(View):
+    def get(self, request):
+        return render(request, "DeustoFixappIncidencia/menu_incidencia.html")
+
+    def list_incidencias(self):
+        buscar = request.POST.get('buscar')
+        incidencias = Incidencia.objects.all()
+        if buscar:
+            incidencias = incidencias.objects.filter(
+                Q(titulo__icontains=buscar) |
+                Q(tipo__icontains=buscar)).distinct()
+        return render(request, 'DeustoFixappIncidencia/list_incidencia.html', {'incidencias': incidencias})
+
+
+# LISTA de incidencia
+class IncidenciaListView(ListView):
     model = Incidencia
-    template_name = 'DeustoFixapptemp/incidencia_list.html'
+    template_name = 'DeustoFixappIncidencia/list_incidencia.html'
+    context_object_name = "incidencias"
 
 
+# CREAR de incidencia
 class IncidenciaCreateView(View):
     def get(self, request):
         formulario = IncidenciaForm()
-        context = {'formulario': formulario}
-        return render(request, 'DeustoFixapptemp/create_incidencia.html')
+        form = {'formulario': formulario}
+        return render(request, 'DeustoFixappIncidencia/create_incidencia.html', form)
+
+    def post(self, request):
+        formulario = IncidenciaForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('menu_incidencia')
+        return render(request, 'DeustoFixappIncidencia/create_incidencia.html', {'formulario': formulario})
+
+
+# DETALLE de incidencia
+class IncidenciaDetailView(View):
+    def get(self, request, pk):
+        incidencia = get_object_or_404(Incidencia, pk=pk)
+        data = {
+            'titulo': incidencia.titulo,
+            'descripcion': incidencia.descripcion,
+            'TIPO': incidencia.tipo,
+            'URGENCIA': incidencia.nivel_urgencia,
+            'estado': incidencia.estado
+        }
+        return JsonResponse(data)
+
+
+# ACTUALIZAR de incidencia
+class IncidenciaUpdateView(View):
+    def get(self, request, pk):
+        incidencia = Incidencia.objects.get(pk=pk)
+        formulario = IncidenciaForm(instance=incidencia)
+
+        form = {'formulario': formulario,
+                'titulo': incidencia
+                }
+        return render(request, 'DeustoFixappIncidencia/update_incidencia.html', form)
+
+    def post(self, request, pk):
+        incidencia = Incidencia.objects.get(pk=pk)
+        formulario = IncidenciaForm(request.POST, instance=incidencia)
+        form = {'formulario': formulario, 'titulo': incidencia}
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('menu_incidencias')
+        return render(request, 'DeustoFixappIncidencia/update_incidencia.html', form)
+
+
+# ELIMINAR de incidencia
+class IncidenciaDeleteView(View):
+    def get(self, request):
+        incidencia_id = request.POST.get('incidencia_id')
+        incidencia = get_object_or_404(Incidencia, pk=incidencia_id)
+        return render(request, 'DeustoFixappIncidencia/menu_incidencia.html', {incidencia: incidencia})
+
+    def post(self, request, pk):
+        incidencia = get_object_or_404(Incidencia, pk=pk)
+        incidencia.delete()
+        return redirect('menu_incidencias')
